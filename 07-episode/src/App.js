3e63dbd0485e2764fb19dd5aab5796e474e73698
neeleshbizoara <bizoaraneelesh@gmail.com>
2025-01-16 23:36:31 +0530

    diff env and routing

--- 07-episode/src/App.js
+++ 07-episode/src/App.js
@@ -1,7 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AppLayout from "./components/AppLayout";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./components/About";
+import ContactUs from "./components/ContactUs";
+import Error from "./components/Error";
+import Body from "./components/Body";
 
-const rootElement = ReactDOM.createRoot(document.getElementById('root'));
+const appRoute = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Body />
+      },
+      {
+        path: "/about",
+        element: <About />
+      },
+      {
+        path: "/contact",
+        element: <ContactUs />
+      }
+    ],
+    errorElement: <Error />
+  }
+]);
 
-rootElement.render(<AppLayout/>);
+const rootElement = ReactDOM.createRoot(document.getElementById("root"));
+
+rootElement.render(<RouterProvider router={appRoute} />);
+// rootElement.render(<AppLayout/>)
--- 07-episode/src/components/Header.js
+++ 07-episode/src/components/Header.js
@@ -1,23 +1,38 @@
-
 import { useState } from "react";
-import logo from "../assets/logo.svg"
+import logo from "../assets/logo.svg";
+import { Link } from "react-router-dom";
 const Header = () => {
-    const [btnName, setBtnName] = useState('Login')
-    return (<div className="header">
-        <div className="logo-container">
-            <img alt="logo" src={logo} className="logoSize" />
-        </div>
-        <div className="nav-items">
-            <ul>
-                <li>Home</li>
-                <li>About Us</li>
-                <li>Contact Us</li>
-                <li>Cart</li>
-                <button className="login" onClick={() => {
-                    btnName === 'Login' ? setBtnName('Logout') : setBtnName('Login')
-                    }}>{btnName}</button>
-            </ul>
-        </div>
-    </div>)
-}
+  const [btnName, setBtnName] = useState("Login");
+  console.log(useState());
+  console.log(process.env.REACT_APP_API_URL, process.env.REACT_APP_ENV);
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img alt="logo" src={logo} className="logoSize" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>
+            <Link to="/"> Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="contact">Contact Us</Link>
+          </li>
+          <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
+            }}
+          >
+            {btnName}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
 export default Header;
